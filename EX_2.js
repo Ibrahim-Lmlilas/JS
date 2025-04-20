3c544387eeec93f912a88dd8bd1f09e90267903d
brahim lmlilas <spedibra@gmail.com>
2025-04-20 14:21:23 +0200

    Vérifier si deux chaînes sont des anagrammes (même lettres, ordre différent).

---
+++ EX_2.js
@@ -0,0 +1,15 @@
+
+// Vérifier si deux chaînes sont des anagrammes (même lettres, ordre différent).
+
+function str(par1,par2) {
+ 
+    
+    if (par1.length === par2.length) {
+        return true
+    }
+    else{
+        return false
+    }
+    
+    
+}
